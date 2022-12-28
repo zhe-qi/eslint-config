bf05ba1a9258c330f087cab30323c98f9c370e65
zhe-qi <zheqi0910@gmail.com>
2022-12-28 13:51:01 +0800

    first commit

---
+++ test/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const bar = async () => {
+  const foo = await import('./index')
+  return {
+    foo,
+  }
+}
+
+
+</script>
+
+<template>
+  <div></div>
+  <div 
+  aria-disabled="asds"
+  asdad 
+  >
+    ad
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
